dd7051937d78a151eedc30884bad6bb04e411986
Jerry Zhang <mowen0303@gmail.com>
2024-11-30 01:22:51 -0500

    0.0.1.11

--- src/pages/searchSuggestion.page.js
+++ src/pages/searchSuggestion.page.js
@@ -3,9 +3,45 @@ import GlobalConfig from '../global';
 
 export default function SearchSuggestionPage() {
 
+    const currentFile = import.meta.url;
+    const currentFileName = currentFile.slice(currentFile.lastIndexOf("/")+1)
+
+
+    /**
+      * * * * * * * * * * * * * * * * * * * * 
+      *          Core code start            * 
+      * * * * * * * * * * * * * * * * * * * * 
+    */
+
     const [query, setQuery] = useState('');
     const [result, setResult] = useState([]);
+
     const repository = ["mobile", "mouse", "moneypot", "monitor", "mousepad"];
+
+    function handelInputChange(event) {
+        setQuery(event.target.value);
+        const result = searchSuggestions(repository, event.target.value)
+        setResult(result)
+    }
+
+    function searchSuggestions(repository, customerQuery) {
+        if (customerQuery.length >= 2) {
+            const filteredResult = repository.filter(x=>{
+                return x.startsWith(customerQuery);
+            })
+            filteredResult.sort();
+            filteredResult.splice(3);
+            return filteredResult;
+        } else {
+            return [];
+        }
+    }
+
+    /**
+      * * * * * * * * * * * * * * * * * * * * 
+      *          Core code end              * 
+      * * * * * * * * * * * * * * * * * * * * 
+    */
 
 
     return (
@@ -33,21 +69,28 @@ export default function SearchSuggestionPage() {
                 <p>repository = ["mobile", "mouse", "moneypot", "monitor", "mousepad"]<br />
                     customerQuery = "mouse"</p>
                 <p><strong>Output:</strong></p>
-                <p>
+                <code>
+                    {
+                    `
                     [
-                    ["mobile", "moneypot", "monitor"],
-                    ["mouse", "mousepad"],
-                    ["mouse", "mousepad"],
-                    ["mouse", "mousepad"],
-                    ["mouse", "mousepad"]
+                        ["mobile", "moneypot", "monitor"],
+                        ["mouse", "mousepad"],
+                        ["mouse", "mousepad"],
+                        ["mouse", "mousepad"],
+                        ["mouse", "mousepad"]
                     ]
-                </p>
+                    `
+                    }
+                    
+                </code>
                 <h2>Explanation</h2>
                 <p>The chain of words that will generate in the search box will be mo, mou, mous, mouse.</p>
                 <p>And each line from the output shows the suggestion of "mo", "mou", "mous", "mouse" respectively in each line.</p>
                 <p>For the keyword suggestions made by the system that are generated for "mo", the matches that will be generated are: ["mobile", "mouse", "moneypot", "monitor", "mousepad"]. Alphabetically, they will be reordered to ["mobile", "moneypot", "monitor", "mouse", "mousepad"]. Thus the keyword suggestions made by the system are ["mobile", "moneypot", "monitor"].</p>
             </div>
             <div className="answerContainer">
+                <p>Try to type "mouse"</p>
+                <p><a target='_blank' href={GlobalConfig.GITHUB_URL+currentFileName}>View Source Code</a></p>
                 <input
                     value={query}
                     onChange={(e) => handelInputChange(e)}
@@ -60,20 +103,4 @@ export default function SearchSuggestionPage() {
             </div>
         </div>
     )
-
-    function handelInputChange(event) {
-        setQuery(event.target.value);
-        const result = searchSuggestions(repository, event.target.value)
-        setResult(result)
-    }
-
-    // const repository =  ["mobile", "mouse", "moneypot", "monitor", "mousepad"];
-    // customerQuery = "mouse"
-    function searchSuggestions(repository, customerQuery) {
-        if (customerQuery.length >= 2) {
-            //const result =
-        } else {
-            return [];
-        }
-    }
 }
